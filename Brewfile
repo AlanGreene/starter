d5b26de94c48cd2f737b66569730f76b7f66ecce
Alan Greene <github.com@alangreene.net>
2015-08-09 01:13:42 +0100

    initial commit for OS X support

---
+++ Brewfile
@@ -0,0 +1,99 @@
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+# $PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+brew 'coreutils'
+# Install some other useful utilities like `sponge`
+#brew 'moreutils'
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew 'findutils'
+# Install GNU `sed`, overwriting the built-in `sed`
+#brew install gnu-sed --default-names
+# Install Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew 'bash'
+#brew 'bash-completion'
+
+tap 'homebrew/dupes'
+tap 'homebrew/versions'
+brew 'homebrew/dupes/grep'
+#brew 'homebrew/dupes/screen'
+brew 'ack'
+brew 'git'
+brew 'openssl'
+brew 'node'
+brew 'unrar'
+brew 'wget'
+brew 'watch'
+brew 'homebrew/dupes/openssh'
+brew 'nmap'
+brew 'curl'
+brew 'heroku-toolbelt'
+# tool to easily access gitignore boilerplates - https://github.com/simonwhitaker/gibo - https://github.com/github/gitignore
+brew 'gibo'
+
+# brew 'ssh-copy-id'
+# brew 'mongodb'
+# brew 'tree'
+
+# http://caskroom.io/search
+# https://caskroomstore.org/
+tap 'caskroom/cask'
+tap 'caskroom/versions'
+brew 'caskroom/cask/brew-cask'
+
+# verify cask install
+# brew cask doctor
+
+cask 'alfred'
+cask 'anvil'
+
+# TODO: need to run the installer after this
+#cask 'backblaze'
+
+#cask 'caffeine'
+#cask 'cheatsheet'
+#cask 'dash'
+#cask 'dropbox'
+#cask 'firefox'
+#cask 'firefox-beta'
+#cask 'fluid'
+#cask 'flux'
+#cask 'google-chrome'
+#cask 'google-chrome-beta'
+#cask 'google-chrome-canary'
+#cask 'grandperspective'
+#cask 'growlnotify'
+#cask 'hazel'
+#cask 'iterm2'
+##cask 'libreoffice'
+##cask 'omnigraffle'
+#cask 'skype'
+#cask 'spectacle'
+#cask 'sublime-text-dev'
+#cask 'the-unarchiver'
+#cask 'transmit'
+#cask 'virtualbox'
+#cask 'vagrant'
+#cask 'vlc'
+# keyboards shortcuts to resize / position windows - https://www.irradiatedsoftware.com/sizeup/
+#cask 'sizeup'
+
+# ## PICK A MOCKUP APP
+# cask 'balsamiq'
+# #cask 'pencil'
+
+# cask 'keepassx'
+
+# cask 'chromecast'
+
+# Install useful Quick Look plugins for developers
+# See https://github.com/sindresorhus/quick-look-plugins
+#cask 'qlcolorcode'
+#cask 'qlstephen'
+#cask 'qlmarkdown'
+#cask 'quicklook-json'
+#cask 'qlprettypatch'
+#cask 'quicklook-csv'
+#cask 'betterzipql'
+#cask 'webp-quicklook'
+#cask 'suspicious-package'
---
+++ bootstrap.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Colours
+# tput setab [1-7] # Set the background colour using ANSI escape
+# tput setaf [1-7] # Set the foreground colour using ANSI escape
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+# Resets the style
+reset=`tput sgr0`
+
+# tput bold    # Select bold mode
+# tput dim     # Select dim (half-bright) mode
+# tput smul    # Enable underline mode
+# tput rmul    # Disable underline mode
+# tput rev     # Turn on reverse video mode
+# tput smso    # Enter standout (bold) mode
+# tput rmso    # Exit standout mode
+
+function cecho() {
+    defaultMsg="No message passed"
+    message=${1:-$defaultMsg}
+    color=${2:-$black}
+
+    echo "$color$message$reset"
+}
+
+set -e
+
+if [ ! -n "$STARTER" ]; then
+    STARTER=~/.starter
+fi
+
+if [ -d "$STARTER" ]; then
+    cecho "You already have the dotfiles starter installed. You'll need to remove $STARTER if you want to install again." $red
+    exit
+fi
+
+# set default platform
+platformFile="$STARTER/.osx"
+
+case "$OSTYPE" in
+    darwin*)
+        cecho "Installing..." $green
+        # Install Xcode command line tools, required by git and others
+        #
+        # the following command opens a software update UI for user interaction so we won't use that
+        #xcode-select --install
+
+        # check if Xcode command line tools are already installed
+        echo "Checking for Xcode command line tools"
+        ! $(xcode-select -p > /dev/null 2>&1) && {
+            #instead we use this neat trick from https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
+            echo "Installing Xcode command line tools"
+            touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+            PROD=$(softwareupdate -l |
+              grep "\*.*Command Line" |
+              head -n 1 | awk -F"*" '{print $2}' |
+              sed -e 's/^ *//' |
+              tr -d '\n')
+            softwareupdate -i "$PROD" -v;
+        }
+        ;;
+    linux*)
+        cecho "This dotfile starter currently only supports OSX. Linux support coming soon. Exiting." $red
+        exit 1
+        ;;
+    *)
+        cecho "Unsupported: $OSTYPE" $red
+        cecho "This dotfile starter currently only supports OSX. Exiting." $red
+        exit 1
+        ;;
+esac
+
+cecho "Cloning dotfiles starter..." $blue
+hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/AlanGreene/starter.git $STARTER || {
+    echo "git not installed"
+    exit
+}
+
+STARTLOC=`pwd`
+cd $STARTER
+
+source $platformFile
+
+cd $STARTLOC
+
+cecho "'\nDotfiles starter installation complete.\n" $green
