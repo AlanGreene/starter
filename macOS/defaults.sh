75336394464a0eea0f48cb4b6902c3eaadf010e7
Alan Greene <github.com@alangreene.net>
2021-04-04 18:02:57 +0100

    Additional preferences
    Activity monitor sort by CPU usage
    
    Disable text replacement
    
    Keyboard repeat improvement

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -12,6 +12,8 @@ defaults write -g AppleShowScrollBars -string "WhenScrolling"
 
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
+# Disable press-and-hold for keys in favor of key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 defaults write -g NSAutomaticCapitalizationEnabled -bool FALSE
 defaults write -g NSAutomaticDashSubstitutionEnabled -bool FALSE
@@ -22,6 +24,12 @@ defaults write -g NSAutomaticTextCompletionEnabled -bool TRUE
 defaults write -g WebAutomaticSpellingCorrectionEnabled -bool FALSE
 defaults write -g NSQuitAlwaysKeepsWindows -bool TRUE
 
+# disable text replacment
+defaults write -g NSUserDictionaryReplacementItems -array '()'
+sqlite3 ~/Library/Dictionaries/CoreDataUbiquitySupport/${USER}~*/UserDictionary/*/store/UserDictionary.db "delete from ZUSERDICTIONARYENTRY;"
+sqlite3 ~/Library/KeyboardServices/TextReplacements.db "delete from ZTEXTREPLACEMENTENTRY;"
+
+## Dock
 # clear the dock and set it to autohide on the right
 defaults delete com.apple.dock persistent-apps
 defaults delete com.apple.dock persistent-others
@@ -51,4 +59,20 @@ defaults write com.apple.dock tilesize -int 50
 defaults write com.apple.dock wvous-tl-corner -int 5
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
-killall Dock
+## Activity Monitor
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+## Kill any affected apps to ensure the settings are applied
+for app in "Activity Monitor" \
+	"cfprefsd" \
+	"Dock" \
+	"Finder" \
+	"SystemUIServer"; do
+	killall "${app}" &> /dev/null
+done
