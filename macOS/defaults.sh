485618fa69a731de759eee6eb41d7439535bc50b
Alan Greene <github.com@alangreene.net>
2021-04-06 18:17:52 +0100

    Dial back the keyrepeat rate a little

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -54,8 +54,8 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 ## Keyboard
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
 # Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 
