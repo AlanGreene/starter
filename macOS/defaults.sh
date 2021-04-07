6bc5d63526765f8b31e3b629e217d1e737178dfb
Alan Greene <github.com@alangreene.net>
2021-04-07 20:47:07 +0100

    Configure Software Update preferences

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -78,6 +78,21 @@ defaults write com.apple.spotlight orderedItems -array \
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
+## Software Update
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -bool true
+# Don't auto install OS updates, system data files & security updates
+defaults write com.apple.SoftwareUpdate AutomaticallyInstallMacOSUpdates -bool false
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -bool false
+# Don't automatically download apps purchased on other Macs
+defaults write com.apple.SoftwareUpdate ConfigDataInstall -bool false
+# Disable app auto-update
+defaults write com.apple.commerce AutoUpdate -bool false
+
 ## Keyboard
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
